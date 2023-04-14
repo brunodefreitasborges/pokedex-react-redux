f171c0f9ef7792c8cda5b5a82dad7616ab8476e1
brunofborges <brunofreitas.ads@gmail.com>
2023-04-13 21:44:07 -0300

    Moved assets to public

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@
 function Header() {
     return (
         <div className="h-16 bg-[#D74526] flex justify-center items-center gap-2">
-            <img src="/src/assets/pokeball.png" alt="" />
+            <img src="/assets/pokeball.png" alt="" />
             <h1 className="sm:text-xl">PokeDex v.2.1</h1>
-            <img src="/src/assets/pokeball.png" alt="" />
+            <img src="/assets/pokeball.png" alt="" />
         </div>
     )
 }
